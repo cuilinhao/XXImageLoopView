901c9b20e392371e6545d54fc2a4ba7aafa48f0b
崔林豪 <lhCui@51tniu.com>
2018-03-28 15:15:01 +0800

    first commit for version 1.0.0

---
+++ XXImageLoopView/XXImageLoopView.h
@@ -0,0 +1,13 @@
+//
+//  XXImageLoopView.h
+//  tttest
+//
+//  Created by 崔林豪 on 2018/3/28.
+//  Copyright © 2018年 cuilinhao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XXImageLoopView : NSObject
+
+@end
---
+++ XXImageLoopView/XXImageLoopView.m
@@ -0,0 +1,13 @@
+//
+//  XXImageLoopView.m
+//  tttest
+//
+//  Created by 崔林豪 on 2018/3/28.
+//  Copyright © 2018年 cuilinhao. All rights reserved.
+//
+
+#import "XXImageLoopView.h"
+
+@implementation XXImageLoopView
+
+@end
